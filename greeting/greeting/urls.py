d09beaf8d9ed808c3391c817af2c787043c87ad8
Eison <eisondanieljohnson007@gmail.com>
2024-12-10 21:12:19 +0530

    file added

--- greeting/greeting/urls.py
+++ greeting/greeting/urls.py
@@ -16,7 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import HelloworldView
+from myapp.views import GoodmorningView
+from myapp.views import GoodafternoonView
+from myapp.views import GoodnightView
+from myapp.views import GoodeveningView
+from myapp.views import MohanlalViwe
+from myapp.views import MammoottyViwe
+from myapp.views import RobertDowneyJrView
+from myapp.views import KamalHaasanView
+from myapp.views import RajinikanthView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("Helloworld/",HelloworldView.as_view()),
+    path("morning/",GoodmorningView.as_view()),
+    path("night/",GoodnightView.as_view()),
+    path("afternoon/",GoodafternoonView.as_view()),
+    path("goodevening/",GoodeveningView.as_view()),
+    path("mohanlal/",MohanlalViwe.as_view()),
+    path("mammootty/",MammoottyViwe.as_view()),
+    path("rdj/",RobertDowneyJrView.as_view()),
+    path("kamal/",KamalHaasanView.as_view()),
+    path("Rejini/",RajinikanthView.as_view()),
+    
+
 ]
--- greeting/myapp/views.py
+++ greeting/myapp/views.py
@@ -1,3 +1,100 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+
+
+class HelloworldView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,"Helloworld.html")
+    
+class GoodmorningView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,"good_morning.html")
+    
+class GoodnightView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,"good_night.html")
+
+class GoodafternoonView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,"good_afternoon.html")
+    
+class GoodeveningView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,"goodevening.html")
+    
+
+
+class MohanlalViwe(View):
+
+    def get(self,request,*args,**kwargs):
+        
+        data={"name":"Mohanlal",
+              "age":64,
+              "number_of_movies":300,
+        }
+
+        return render(request,"mohanlal.html",data)
+    
+
+class MammoottyViwe(View):
+
+    def get(self,request,*args,**kwargs):
+        
+        data={"name":"Mammootty",
+              "age":73,
+              "number_of_movies":400,
+        }
+
+        return render(request,"mammootty.html",data)
+    
+class RobertDowneyJrView(View):
+    def get(self,request,*args,**kwargs):
+
+        data={"name":"Robert John Downey Jr",
+              "age":59,
+              "number_of_movies":95,
+        }
+
+        return render(request,"rdj.html",data)
+    
+
+class KamalHaasanView(View):
+    def get(self,request,*args,**kwargs):
+
+        data={"name":"Kamal Haasan",
+              "age":70,
+              "number_of_movies":200,
+        }
+
+        return render(request,"Kamal_Haasan.html",data)
+    
+class RajinikanthView(View):
+    def get(self,request,*args,**kwargs):
+
+        data={"name":"Rajinikanth",
+              "age":73,
+              "number_of_movies":170,
+        }
+
+        return render(request,"Rajinikanth.html",data)
+
+
+
+    # Kamal Haasan
+
+    # Rajinikanth
+
+
+
