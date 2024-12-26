29032c23912d9599b0df7d931f65ccc9b2e63be2
Eison <eisondanieljohnson007@gmail.com>
2024-12-26 12:23:05 +0530

    added html\

--- BookCrud/store/views.py
+++ BookCrud/store/views.py
@@ -4,3 +4,34 @@ from django.shortcuts import render
 
 from django.views.generic import View
 
+from store.forms import BookForm
+
+from store.models import Book
+
+class BookCreateView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        form_instance=BookForm()
+
+        return render(request,"book_add.html",{"form":form_instance})
+    
+    def post(self,request,*args,**kwargs):
+
+        form_data=request.POST
+
+        form_instance=BookForm(form_data)
+
+        if form_instance.is_valid():
+
+            data=form_instance.cleaned_data
+
+            Book.objects.create(
+                title=data.get("title"),
+                auther=data.get("auther"),
+                price=data.get("price"),
+                genre=data.get("genre"),
+                language=data.get("language"),
+                year=data.get("year")
+            )
+        return render(request,"book_add.html",{"form":form_instance})
